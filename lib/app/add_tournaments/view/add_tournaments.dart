2ff09a328a41c13a197c321c4dc1403222d5ce72
pranavpv773 <pranavpv773@gmail.com>
2022-09-19 23:07:30 +0530

    list all worked

--- lib/app/add_tournaments/view/add_tournaments.dart
+++ lib/app/add_tournaments/view/add_tournaments.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:tournament_lotter/app/add_tournaments/view/widgets/terxtformwidget.dart';
+import 'package:tournament_lotter/app/add_tournaments/view_model/add_provider.dart';
 import 'package:tournament_lotter/app/constants/colors/colors.dart';
 
 class AddTournamentScreen extends StatelessWidget {
@@ -17,9 +19,11 @@ class AddTournamentScreen extends StatelessWidget {
         title: Text(title == "null" ? 'Add Tournament' : title),
         centerTitle: true,
       ),
-      body: ListView(
-        children: [
-          GridView.builder(
+      body: Form(
+        key: context.read<AddTournamentProvider>().addTournamentKey,
+        child: ListView(
+          children: [
+            GridView.builder(
               padding: const EdgeInsets.symmetric(vertical: 16.0),
               physics: const ScrollPhysics(),
               shrinkWrap: true,
@@ -33,9 +37,26 @@ class AddTournamentScreen extends StatelessWidget {
               itemBuilder: (BuildContext ctx, index) {
                 return TextformWidget(
                   text: 'Team ${index + 1}',
+                  controller:
+                      context.read<AddTournamentProvider>().controllers[index],
                 );
-              }),
-        ],
+              },
+            ),
+            Center(
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.amber)),
+                onPressed: () {
+                  context.read<AddTournamentProvider>().createTeams(length);
+                },
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 50),
+                  child: Text("ADD TEAMS"),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/app/add_tournaments/view_model/add_provider.dart
+++ lib/app/add_tournaments/view_model/add_provider.dart
@@ -1,11 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class AddTournamentProvider with ChangeNotifier {
   String? choice;
   String? radioValue;
   int? selectTeams;
   Color? color;
+
+  List<TextEditingController> controllers = [];
+  final addTournamentKey = GlobalKey<FormState>();
+  List<String> teams = [];
+  List<String> teamsA = [];
+  List<String> teamsB = [];
+  createTeams(int limit) async {
+    for (int i = 0; i < limit; i++) {
+      teams.add(controllers[i].text);
+      controllers[i].clear();
+    }
+    teams.shuffle();
+    double half = teams.length / 2;
+    createMatches(half.toInt());
+  }
+
+  createMatches(int limit) async {
+    teamsA.addAll(teams.sublist(0, limit));
+    teamsB.addAll(teams.sublist(limit));
+    print(teamsA);
+    print(teamsB);
+
+    teams.shuffle();
+  }
+
+  createController(int limit) {
+    controllers = List.generate(limit, (i) => TextEditingController());
+  }
+
   void selectTeam(int value) {
     selectTeams = value;
     notifyListeners();
