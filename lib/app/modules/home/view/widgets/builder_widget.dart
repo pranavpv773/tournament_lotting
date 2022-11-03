af58de98a975dc94b46f6312e478f672370a5ff9
pranavpv773 <pranavpv773@gmail.com>
2022-11-03 21:16:21 +0530

    projects screen completed

--- lib/app/modules/home/view/widgets/builder_widget.dart
+++ lib/app/modules/home/view/widgets/builder_widget.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:git_app/app/app_styles/app_colors.dart';
 import 'package:git_app/app/app_styles/app_text_styles.dart';
 import 'package:git_app/app/modules/home/view_model/home_provider.dart';
+import 'package:git_app/app/modules/project/view/project_screen.dart';
+import 'package:git_app/routes/routes.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
@@ -22,135 +24,148 @@ class CardbuilderWidget extends StatelessWidget {
             itemBuilder: (context, index) {
               return Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Card(
-                  elevation: 10,
-                  shadowColor: AppColors.primary1,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.all(10),
-                        width: 120,
-                        height: 150,
-                        decoration: BoxDecoration(
-                          color: AppColors.primary,
-                          borderRadius: BorderRadius.circular(20),
-                          image: DecorationImage(
-                            image: NetworkImage(
-                              val.itemList[index].owner!.avatarUrl.toString(),
+                child: GestureDetector(
+                  onTap: () => Routes.nextScreen(
+                      screen: ProjectScreen(
+                    index: index,
+                    image: val.itemList[index].owner!.avatarUrl.toString(),
+                  )),
+                  child: Card(
+                    elevation: 10,
+                    shadowColor: AppColors.primary1,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.all(10),
+                          width: 120,
+                          height: 150,
+                          decoration: BoxDecoration(
+                            color: AppColors.primary,
+                            borderRadius: BorderRadius.circular(20),
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                val.itemList[index].owner!.avatarUrl.toString(),
+                              ),
+                              fit: BoxFit.fitHeight,
                             ),
-                            fit: BoxFit.fitHeight,
                           ),
                         ),
-                      ),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const SizedBox(
-                              height: 5,
-                            ),
-                            Text(
-                              val.itemList[index].name.toString(),
-                              style: AppTextStyles.h3.copyWith(
-                                  color: AppColors.kBlack,
-                                  letterSpacing: 1,
-                                  wordSpacing: 1.5,
-                                  overflow: TextOverflow.ellipsis,
-                                  fontSize: 16),
-                            ),
-                            const SizedBox(
-                              height: 5,
-                            ),
-                            Text(
-                              val.itemList[index].description != null
-                                  ? val.itemList[index].description.toString()
-                                  : "No discription",
-                              style: AppTextStyles.h3.copyWith(
-                                  overflow: TextOverflow.ellipsis,
-                                  color: AppColors.kGrey,
-                                  fontSize: 13),
-                            ),
-                            const SizedBox(
-                              height: 5,
-                            ),
-                            Container(
-                              width: 120,
-                              height: 30,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: const Color.fromARGB(255, 108, 234, 113),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const SizedBox(
+                                height: 5,
                               ),
-                              child: Center(
-                                child: Text(
-                                  val.itemList[index].language != null
-                                      ? val.itemList[index].language.toString()
-                                      : "No Language",
-                                  style: AppTextStyles.h3.copyWith(
-                                      color: const Color.fromARGB(
-                                        255,
-                                        95,
-                                        130,
-                                        97,
-                                      ),
-                                      fontSize: 13),
+                              Text(
+                                val.itemList[index].name.toString(),
+                                style: AppTextStyles.h3.copyWith(
+                                    color: AppColors.kBlack,
+                                    letterSpacing: 1,
+                                    wordSpacing: 1.5,
+                                    overflow: TextOverflow.ellipsis,
+                                    fontSize: 16),
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                val.itemList[index].description != null
+                                    ? val.itemList[index].description.toString()
+                                    : "No discription",
+                                style: AppTextStyles.h3.copyWith(
+                                    overflow: TextOverflow.ellipsis,
+                                    color: AppColors.kGrey,
+                                    fontSize: 13),
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Container(
+                                width: 120,
+                                height: 30,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color:
+                                      const Color.fromARGB(255, 108, 234, 113),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    val.itemList[index].language != null
+                                        ? val.itemList[index].language
+                                            .toString()
+                                        : "No Language",
+                                    style: AppTextStyles.h3.copyWith(
+                                        color: const Color.fromARGB(
+                                          255,
+                                          95,
+                                          130,
+                                          97,
+                                        ),
+                                        fontSize: 13),
+                                  ),
                                 ),
                               ),
-                            ),
-                            const SizedBox(
-                              height: 5,
-                            ),
-                            Text(
-                              "Total Stars: ${val.itemList[index].stargazersCount.toString()}⭐",
-                              style: AppTextStyles.h3.copyWith(
-                                  color: AppColors.kBlack, fontSize: 13),
-                            ),
-                            const SizedBox(
-                              height: 5,
-                            ),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 8.0),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    "👁️${val.itemList[index].watchersCount!.toString().substring(2)}k",
-                                    style: AppTextStyles.h3.copyWith(
-                                        color: AppColors.kBlack, fontSize: 13),
-                                  ),
-                                  Text(
-                                    DateFormat('dd-MM-yyyy')
-                                        .format(val.itemList[index].createdAt!)
-                                        .toString(),
-                                    style: AppTextStyles.h3.copyWith(
-                                        color: AppColors.kBlack, fontSize: 13),
-                                  ),
-                                  Text(
-                                    val.itemList[index].defaultBranch != null
-                                        ? val.itemList[index].defaultBranch
-                                            .toString()
-                                            .substring(14)
-                                        : "No branch",
-                                    style: AppTextStyles.h3.copyWith(
-                                        color: AppColors.kBlack, fontSize: 13),
-                                  ),
-                                ],
+                              const SizedBox(
+                                height: 5,
                               ),
-                            ),
-                            const SizedBox(
-                              height: 5,
-                            ),
-                          ],
+                              Text(
+                                "Total Stars: ${val.itemList[index].stargazersCount.toString()}⭐",
+                                style: AppTextStyles.h3.copyWith(
+                                    color: AppColors.kBlack, fontSize: 13),
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 8.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "👁️${val.itemList[index].watchersCount!.toString().substring(2)}k",
+                                      style: AppTextStyles.h3.copyWith(
+                                          color: AppColors.kBlack,
+                                          fontSize: 13),
+                                    ),
+                                    Text(
+                                      DateFormat('dd-MM-yyyy')
+                                          .format(
+                                              val.itemList[index].createdAt!)
+                                          .toString(),
+                                      style: AppTextStyles.h3.copyWith(
+                                          color: AppColors.kBlack,
+                                          fontSize: 13),
+                                    ),
+                                    Text(
+                                      val.itemList[index].defaultBranch != null
+                                          ? val.itemList[index].defaultBranch
+                                              .toString()
+                                              .substring(14)
+                                          : "No branch",
+                                      style: AppTextStyles.h3.copyWith(
+                                          color: AppColors.kBlack,
+                                          fontSize: 13),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               );
---
+++ lib/app/modules/project/view/project_screen.dart
@@ -0,0 +1,282 @@
+import 'package:flutter/material.dart';
+import 'package:git_app/app/app_styles/app_colors.dart';
+import 'package:git_app/app/app_styles/app_text_styles.dart';
+import 'package:git_app/app/modules/home/view_model/home_provider.dart';
+import 'package:git_app/routes/routes.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+
+class ProjectScreen extends StatelessWidget {
+  const ProjectScreen({Key? key, required this.image, required this.index})
+      : super(key: key);
+  final String image;
+  final int index;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(
+                color: AppColors.primary1,
+                image: DecorationImage(
+                  image: NetworkImage(image),
+                  fit: BoxFit.fill,
+                ),
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 60),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CircleAvatar(
+                      radius: 20,
+                      backgroundColor: AppColors.primary,
+                      child: IconButton(
+                        onPressed: () => Routes.backScreen(),
+                        icon: Icon(
+                          Icons.arrow_back,
+                          color: AppColors.primary1,
+                        ),
+                      ),
+                    ),
+                    CircleAvatar(
+                      radius: 20,
+                      backgroundColor: AppColors.primary,
+                      child: Icon(
+                        Icons.more_vert,
+                        color: AppColors.primary1,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            bottomDetailsSheet(index),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget bottomDetailsSheet(int index1) {
+  return DraggableScrollableSheet(
+    initialChildSize: 0.5,
+    minChildSize: 0.020,
+    maxChildSize: .86,
+    builder: (BuildContext context, ScrollController scrollController) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 5.0),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(10),
+              topRight: Radius.circular(
+                10,
+              ),
+            ),
+            color: AppColors.primary,
+          ),
+          child: ListView(
+            controller: scrollController,
+            children: [
+              ListTile(
+                title: Text(
+                  "Name",
+                  style: AppTextStyles.h3
+                      .copyWith(color: AppColors.kBlack, letterSpacing: 1),
+                ),
+                subtitle: Text(
+                  context
+                      .read<HomeNotifier>()
+                      .itemList[index1]
+                      .name
+                      .toString()
+                      .toUpperCase(),
+                  style: AppTextStyles.h3
+                      .copyWith(color: AppColors.kBlack, letterSpacing: 1),
+                ),
+              ),
+              ListTile(
+                title: Text(
+                  "Description",
+                  style: AppTextStyles.h3
+                      .copyWith(color: AppColors.kBlack, letterSpacing: 1),
+                ),
+                subtitle: Text(
+                  context
+                      .read<HomeNotifier>()
+                      .itemList[index1]
+                      .description
+                      .toString(),
+                  style: AppTextStyles.h2.copyWith(
+                    color: AppColors.kGrey,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              ExpansionTileWidget(
+                index1: index1,
+              ),
+              ListTile(
+                title: Text(
+                  "Fork",
+                  style: AppTextStyles.h3
+                      .copyWith(color: AppColors.kBlack, letterSpacing: 1),
+                ),
+                subtitle: Text(
+                  context.read<HomeNotifier>().itemList[index1].fork.toString(),
+                  style: AppTextStyles.h2.copyWith(
+                    color: AppColors.kGrey,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text(
+                  "Watchers",
+                  style: AppTextStyles.h3
+                      .copyWith(color: AppColors.kBlack, letterSpacing: 1),
+                ),
+                subtitle: Text(
+                  context
+                      .read<HomeNotifier>()
+                      .itemList[index1]
+                      .watchersCount
+                      .toString(),
+                  style: AppTextStyles.h2.copyWith(
+                    color: AppColors.kGrey,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text(
+                  "Create at",
+                  style: AppTextStyles.h3
+                      .copyWith(color: AppColors.kBlack, letterSpacing: 1),
+                ),
+                subtitle: Text(
+                  DateFormat('dd-MM-yyyy')
+                      .format(context
+                          .read<HomeNotifier>()
+                          .itemList[index1]
+                          .createdAt!)
+                      .toString(),
+                  style: AppTextStyles.h2.copyWith(
+                    color: AppColors.kGrey,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text(
+                  "Homepage Url",
+                  style: AppTextStyles.h3
+                      .copyWith(color: AppColors.kBlack, letterSpacing: 1),
+                ),
+                subtitle: Text(
+                  context
+                      .read<HomeNotifier>()
+                      .itemList[index1]
+                      .homepage
+                      .toString(),
+                  style: AppTextStyles.h2.copyWith(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text(
+                  "Issues",
+                  style: AppTextStyles.h3
+                      .copyWith(color: AppColors.kBlack, letterSpacing: 1),
+                ),
+                subtitle: Text(
+                  context
+                      .read<HomeNotifier>()
+                      .itemList[index1]
+                      .hasIssues
+                      .toString(),
+                  style: AppTextStyles.h2.copyWith(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+}
+
+class ExpansionTileWidget extends StatelessWidget {
+  const ExpansionTileWidget({
+    Key? key,
+    required this.index1,
+  }) : super(key: key);
+  final int index1;
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+        backgroundColor: AppColors.primary1,
+        trailing: const Icon(Icons.add),
+        title: Text(
+          "The Projects Includes",
+          style: AppTextStyles.h2.copyWith(
+            letterSpacing: 1.5,
+            fontWeight: FontWeight.bold,
+            color: AppColors.kBlack,
+          ),
+        ),
+        children: [
+          context.read<HomeNotifier>().itemList[index1].topics!.isEmpty
+              ? Text(
+                  "No topics",
+                  style: AppTextStyles.h3,
+                )
+              : Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GridView.builder(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 16.0,
+                    ),
+                    physics: const ScrollPhysics(),
+                    shrinkWrap: true,
+                    gridDelegate:
+                        const SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: 150,
+                      mainAxisExtent: 30,
+                      crossAxisSpacing: 20,
+                      mainAxisSpacing: 20,
+                    ),
+                    itemCount: context
+                        .read<HomeNotifier>()
+                        .itemList[index1]
+                        .topics!
+                        .length,
+                    itemBuilder: (BuildContext ctx, index) {
+                      final data =
+                          context.read<HomeNotifier>().itemList[index1].topics;
+                      return Text(
+                        "👨‍💻  ${data![index].toString().toUpperCase()}",
+                        style: AppTextStyles.h3.copyWith(
+                          fontSize: 12,
+                          color: AppColors.primary,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+        ]);
+  }
+}
